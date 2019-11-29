475f1b8970db918cad21815f9e84db27c4f04cca
Nick Corona <coronaisme@gmail.com>
2019-11-29 23:34:11 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -19,6 +19,7 @@ def hopper
 programmer_hash[:grace_hopper]
 end
 hopper
+
 def alan_kay_is_known_for
 	# What combination of keys would you use to return the value of the :known_for key of :alan_kay?
 	
@@ -37,7 +38,9 @@ def alan_kay_is_known_for
           :languages => ["C"]
         }
      }
+     programmer_hash[:alan_kay][:known_for]
 end
+alan_kay_is_known_for
 
 def dennis_ritchies_language
 	programmer_hash = 
