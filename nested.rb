21ed96235bf9f85580f4e89663194053abb6e1e7
Nick Corona <coronaisme@gmail.com>
2019-11-29 23:52:59 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -132,7 +132,7 @@ def adding_to_dennis
           :languages => ["C"]
         }
      }
-     new_language = "Assembly"
-     programmer_hash[:dennis_ritchie][:]
-
+     new_language = ["C","Assembly"]
+     programmer_hash[:dennis_ritchie][:languages] = new_language
+     return programmer_hash
 end
